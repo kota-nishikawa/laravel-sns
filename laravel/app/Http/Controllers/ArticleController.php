12b969261c7ae6622f07546048183658f1af8108
Kota Nishikawa <kota.nishikawa@site4d.com>
2020-11-06 08:02:10 +0900

    メールアドレス忘れの問い合わせをできるようにする

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -6,11 +6,16 @@ use Illuminate\Http\Request;
 
 use App\Article;
 
+use App\Http\Requests\ArticleRequest;
 
 class ArticleController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Article::class, 'article');
+    }
 
-    //==========ここから追加==========
+
     public function index()
     {
 
@@ -19,9 +24,33 @@ class ArticleController extends Controller
 
         return view('articles.index', ['articles' => $articles]);
     }
-    //==========ここまで追加==========
-   public function create()
+     public function show(Article $article)
+    {
+        return view('articles.show', ['article' => $article]);
+    }
+
+    public function create()
     {
         return view('articles.create');
+    }
+
+
+    public function store(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all());
+
+        $article->user_id = $request->user()->id;
+        $article->save();
+        return redirect()->route('articles.index');
+    }
+    public function edit(Article $article)
+    {
+        return view('articles.edit', ['article' => $article]);
+    }
+
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return redirect()->route('articles.index');
     }
 }
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
 Auth::routes();
-Route::get('/', 'ArticleController@index')->name('articles.index'); //-- この行を編集
-Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth'); //-- この行を変更
+Route::get('/', 'ArticleController@index')->name('articles.index');
+Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
+Route::resource('/articles', 'ArticleController')->only(['show']);
